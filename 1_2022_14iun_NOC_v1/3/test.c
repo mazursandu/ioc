60e66bf5aad661a08d7ceeb56b1d5f86c92ed10b
Alexandru Mazur <alexandru.mazur@code11.com>
2022-09-05 21:41:09 +0300

    fixed

--- 1_2022_14iun_NOC_v1/3/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-
-  // TODO c)
-  starship("abc TREB","TREB");
-	return 0;
-}
--- 3_2022_20iun_NOC_v2/3/object_attack.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    /* call hidden function from object.o with the "key1 key2" argument */
-
-    return 0;
-}
